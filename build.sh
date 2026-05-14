368aae926649463445f52481911f2ec12916ec73
dewet99 <dewet.denkema@gmail.com>
2026-05-14 17:45:14 +0200

    Added faster running animation

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 dotnet run --project ./CakeBuild/CakeBuild.csproj -- "$@"
-cp ~/Documents/projects/VintageStoryMods/fields_of_cranberries_vsmod/Releases/fieldsofcranberries_0.0.1.zip ~/.config/VintagestoryData/Mods
+# Find the latest fieldsofcranberries zip file and copy it to the Mods directory
+latest_zip=$(ls -t ~/Documents/projects/VintageStoryMods/fields_of_cranberries_vsmod/Releases/fieldsofcranberries_*.zip 2>/dev/null | head -n1)
+if [ -n "$latest_zip" ]; then
+    cp "$latest_zip" ~/.config/VintagestoryData/Mods/
+    echo "Copied $(basename "$latest_zip") to Mods directory"
+else
+    echo "No fieldsofcranberries zip file found in Releases directory"
+fi
